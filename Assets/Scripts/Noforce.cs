2ae710d399b9ce324fc8e92fb43cf99d856c14a3
Eric <yerick147083@gmail.com>
2017-11-24 09:00:24 -0600

    Push before oncoming restart

---
+++ Assets/Scripts/Noforce.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Noforce : MonoBehaviour {
+	public Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void FixedUpdate()
+	{
+		rb.velocity = new Vector3(0, 0, 0);
+		rb.angularVelocity = new Vector3(0, 0, 0);
+	}
+}
